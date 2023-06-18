4cd95e2c112d1a291af51086e6500736d1a94137
KISLY74 <kisly.nikita@mail.ru>
2023-06-18 13:53:51 +0300

    init

--- service/tokenService.js
+++ service/tokenService.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken')
 const { Token } = require('../models/models')
-
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
